fdb4381e58679e257a4071b3612e9eb09abdc29e
venkatesh2299 <67502824+venkatesh2299@users.noreply.github.com>
2022-07-09 18:31:46 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws"{
 region = "us-east-1"
-access_key = "<access_key>"
-secret_key = "<secret_key>"
+access_key = "AKIAT43R4RHJ5K2PXJ2D"
+secret_key = "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"
 }
